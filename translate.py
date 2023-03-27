aaf8524ad98267baaa37b1f6669d43531e35d568
Hannah Walton <hmw0069@auburn.edu>
2023-03-26 23:39:53 -0500

    4/6 functions working :/.

--- translate.py
+++ translate.py
@@ -90,6 +90,8 @@ def get_all_translations(rna_sequence, genetic_code):
                 break
             if genetic_code[codon] == "*":
                 break
+            if rna_sequence[0:2] != "AUG":
+                break
             else:
                 amino_acids += genetic_code[codon]
             amino_acids_list.append(amino_acids)
@@ -187,6 +189,22 @@ def get_longest_peptide(rna_sequence, genetic_code):
     """
     pass
 
+    rna_sequence = rna_sequence.upper()
+    if len(rna_sequence) < 3 or rna_sequence[0:2] == "*" in genetic_code:
+        return ""
+    else:
+        amino_acids = []
+        amino_acids_list = []
+        for base in range(0, len(rna_sequence), 3):
+            codon = rna_sequence[base:base+3]
+            if len(codon) < 3:
+                break
+            if genetic_code[codon] == "*":
+                break
+            else:
+                amino_acids += genetic_code[codon]
+            amino_acids_list.append(amino_acids)
+        return amino_acids_list
 
 if __name__ == '__main__':
     genetic_code = {'GUC': 'V', 'ACC': 'T', 'GUA': 'V', 'GUG': 'V', 'ACU': 'T', 'AAC': 'N', 'CCU': 'P', 'UGG': 'W', 'AGC': 'S', 'AUC': 'I', 'CAU': 'H', 'AAU': 'N', 'AGU': 'S', 'GUU': 'V', 'CAC': 'H', 'ACG': 'T', 'CCG': 'P', 'CCA': 'P', 'ACA': 'T', 'CCC': 'P', 'UGU': 'C', 'GGU': 'G', 'UCU': 'S', 'GCG': 'A', 'UGC': 'C', 'CAG': 'Q', 'GAU': 'D', 'UAU': 'Y', 'CGG': 'R', 'UCG': 'S', 'AGG': 'R', 'GGG': 'G', 'UCC': 'S', 'UCA': 'S', 'UAA': '*', 'GGA': 'G', 'UAC': 'Y', 'GAC': 'D', 'UAG': '*', 'AUA': 'I', 'GCA': 'A', 'CUU': 'L', 'GGC': 'G', 'AUG': 'M', 'CUG': 'L', 'GAG': 'E', 'CUC': 'L', 'AGA': 'R', 'CUA': 'L', 'GCC': 'A', 'AAA': 'K', 'AAG': 'K', 'CAA': 'Q', 'UUU': 'F', 'CGU': 'R', 'CGC': 'R', 'CGA': 'R', 'GCU': 'A', 'GAA': 'E', 'AUU': 'I', 'UUG': 'L', 'UUA': 'L', 'UGA': '*', 'UUC': 'F'}
